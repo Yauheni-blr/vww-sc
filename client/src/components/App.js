0a9a88ff35adc0c881376505f9ca45667b5036d3
romacold <romacold@gmail.com>
2017-06-28 13:11:13 +0200

    Footer expoted

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
+import { Footer } from './Footer/Footer'
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Cool Site
+      <div className="app">
+        <Footer />
       </div>
     );
   }
