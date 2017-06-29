bd6a0affdf10dc43920219424a23b879bd05f3e3
romacold <romacold@gmail.com>
2017-06-29 14:06:45 +0200

    yepta

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,6 +3,9 @@ import React, { Component } from 'react'
 import Header from './Header/Header'
 import Body from './Body/Body'
 import { Footer } from './Footer/Footer'
+import LoginForm from './LoginForm/LoginForm'
+
+import AppStore from '../store/AppStore'
 
 import './App.css'
 
@@ -11,15 +14,14 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <Header
-          projectName="Super Puper Duper Project"
-        />
-        
+        <Header />
+
         <Body
           url="https://www.youtube.com/embed/gCcx85zbxz4"
         />
-        
+        <LoginForm/>
         <Footer />
+
       </div>
     );
   }
--- client/src/store/AppStore.js
+++ client/src/store/AppStore.js
@@ -1,3 +1,25 @@
-export const AppStore = {
-    
+import { observable, action } from 'mobx'
+
+import Config from '../Config'
+
+class AppStore extends Config {
+    @observable showLoginModal
+    @observable projectName
+
+
+    constructor() {
+      super()
+      this.showLoginModal = false
+      this.projectName = 'Cool Project'
+    }
+
+    @action setShowLoginModal(value = false) {
+      this.showLoginModal = value
+    }
+
+    @action setProjectName(value) {
+      this.projectName = value
+    }
+
 }
+export default new AppStore()
