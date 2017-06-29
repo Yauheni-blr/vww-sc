da6300b985e2284123509bbc7211e899fdfa3fa4
Владислав <vladislav@MacBook-Air-Vladislav.local>
2017-06-29 12:27:32 +0200

    Modal window added

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,23 +1,36 @@
 import React, { Component } from 'react'
+import { inject, observer } from 'mobx-react'
+
 
 import './Header.css'
 
+@inject('app') @observer
 export default class Header extends Component {
+  constructor(props) {
+    super(props)
+
+    this.handleClick = this.handleClick.bind(this)
+  }
     render() {
       return (
           <div className="app__header">
             <div className="app__header__logo">
               <span className="app__header__logo-item">
-                { this.props.projectName }
+                { this.props.app.projectName }
               </span>
             </div>
 
             <div className="app__header__auth">
-              <button className="app__header__auth-item">
+              <button className="app__header__auth-item" onClick={ this.handleClick }>
                 Log in
               </button>
             </div>
           </div>
         )
+    }
+
+
+    handleClick() {
+      this.props.app.setShowLoginModal(true, 'filter', 'blur(12px')
     }
 }
--- client/src/store/AppStore.js
+++ client/src/store/AppStore.js
@@ -1,3 +1,29 @@
-export const AppStore = {
+import { observable, action } from 'mobx'
+
+class AppStore {
+  @observable showLoginModal
+  @observable projectName
     
+  constructor() {
+    this.showLoginModal = {
+      status: false,
+      addStyle: {}
+    }
+    this.projectName = "cool project"
+  }
+
+  @action setShowLoginModal(status = false, styleKey = null, styleValue = null) {
+    this.showLoginModal = {
+      status,
+      addStyle: {
+        [styleKey]: styleValue
+      }
+    }
+  }
+
+  @action setProjectName(value) {
+    this.projectName = value
+  }
 }
+
+export default new AppStore()
